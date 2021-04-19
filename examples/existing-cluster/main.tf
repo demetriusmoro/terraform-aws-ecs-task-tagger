06d71504b3ebde028f1a67d0ef862b2934c3db77
Demetrius <demetrius.moro@gmail.com>
2021-04-19 01:10:02 -0300

    New version release

--- examples/existing-cluster/main.tf
+++ examples/existing-cluster/main.tf
@@ -1,5 +1,5 @@
 module "my_ecs_task_tagger" {
   source       = "demetriusmoro/ecs-task-tagger/aws"
-  version      = "0.0.4"
+  version      = "0.0.6"
   cluster_name = "my-existing-cluster-name"
 }
